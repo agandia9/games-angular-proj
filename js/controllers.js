8db4dab8e4158a146636df16dfc7959aff986b92
Alejandro Delgado Gandia <alejandro.gandia9@gmail.com>
2017-05-19 15:25:52 +0200

    orderby..

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,6 @@
 var app = angular.module('gameApp')
 app.controller('DataController', function($scope, GamesFactory, $location) {
+
 	$scope.games = function(gameTitle) {
 		console.log("from controller.. we search by => " + $scope.gameTitle)
 		$scope.gamesInfo = GamesFactory.getGames($scope.gameTitle)
@@ -23,14 +24,11 @@ app.controller('DataController', function($scope, GamesFactory, $location) {
 		.then(function(response) {
 			console.log(response.data.results)
 			$scope.infoAboutGames = response.data.results
-
 			for(prop in $scope.infoAboutGames){
 				if (prop === 'genres') {
 					for (var i = 0; i < $scope.infoAboutGames[prop].length; i++) {
-						console.log(`call ${i} times`)
 						$scope.infoAboutGames[prop][i].name = GamesFactory.getIcon($scope.infoAboutGames[prop][i].name)
 					};
-					
 				}
 			}
 		})
--- js/factory.js
+++ js/factory.js
@@ -1,80 +1,87 @@
 angular.module('GamesService', [])
-	.factory('GamesFactory', function($http, $location) {
-		var getGames = function(gameTitle) {
-			console.log("From Factory we recived => " + gameTitle)
-			return $http.post('http://localhost:9000/getInfo', {"gameTitle":gameTitle} )
+.factory('GamesFactory', function($http, $location) {
+	var getGames = function(gameTitle) {
+		console.log("From Factory we recived => " + gameTitle)
+		return $http.post('http://localhost:9000/getInfo', {"gameTitle":gameTitle} )
+				//now works because angular version...
 				// .error(function(data) {
 				// 	console.log('Error: ' + data);
 				// });
-		}
-		var getInfo = function(gameId){
-			console.log( gameId)
-			return $http.post('http://localhost:9000/getMoreInfo', {"gameId":gameId} )
-				// .error(function(data) {
-				// 	console.log('Error: ' + data);
-				// });
-		}
-
-		var getIcon = function(infoAboutGames){
-			console.log(infoAboutGames)
-							if(infoAboutGames === 'Driving/Racing'){
-								return `🏎 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Puzzle'){
-								return  `⛏ ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Adventure'){
-								return `🏇🏼🎒 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Platformer'){
-								return `🏃🏽 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Action'){
-								return `🏹 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Shooter'){
-								return `🔫 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Role-Playing'){
-								return `🗡 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Fighting'){
-								return `👊🏼 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Vehicular Combat'){
-								return `🏎💣 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Action-Adventure'){
-								return `💥💣 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Strategy'){
-								return `🤔 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Real-Time Strategy'){
-								return `🤔⏳ ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Simulation'){
-								return `👥 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Flight Simulator'){
-								return `✈️ ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'MOBA'){
-								return `🌍 ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'Card Game'){
-								return `♠️♣️♥️♦️ ${infoAboutGames}`
-							}
-							else if(infoAboutGames === 'MMORPG'){
-								return `🌍🗡 ${infoAboutGames}`
-							}
-							else {
-								return `⁉️  ${infoAboutGames}`
-							}
 			}
-		return {
-			getIcon: getIcon,
-			getGames: getGames,
-			getInfo: getInfo
-		}
-	})
+			var getInfo = function(gameId){
+				console.log( gameId)
+				return $http.post('http://localhost:9000/getMoreInfo', {"gameId":gameId} )
+				// .error(function(data) {
+				// 	console.log('Error: ' + data);
+				// });
+			}
+			var getIcon = function(infoAboutGames){
+				console.log(infoAboutGames)
+				if(infoAboutGames === 'Driving/Racing'){
+					return `🏎 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Puzzle'){
+					return `⛏ ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Adventure'){
+					return `🏇🏼🎒 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Platformer'){
+					return `🏃🏽 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Action'){
+					return `🏹 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Shooter'){
+					return `🔫 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Role-Playing'){
+					return `🗡 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Fighting'){
+					return `👊🏼 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Vehicular Combat'){
+					return `🏎💣 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Action-Adventure'){
+					return `💥💣 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Strategy'){
+					return `🤔 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Real-Time Strategy'){
+					return `🤔⏳ ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Simulation'){
+					return `👥 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Flight Simulator'){
+					return `✈️ ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'MOBA'){
+					return `🌍 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Card Game'){
+					return `♠️♣️♥️♦️ ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'MMORPG'){
+					return `🌍🗡 ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Compilation'){
+					return `🗄➕ ${infoAboutGames}`
+				}
+				else if(infoAboutGames === 'Music/Rhythm'){
+					return `🎵🎤 ${infoAboutGames}`
+				}
+				
+				else {
+					return `⁉️  ${infoAboutGames}`
+				}
+			}
+			return {
+				getIcon: getIcon,
+				getGames: getGames,
+				getInfo: getInfo
+			}
+		})

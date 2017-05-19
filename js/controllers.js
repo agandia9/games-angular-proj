21c556f97c2d6095bbd29b4bb2c30e927d8bed1c
Alejandro Delgado Gandia <alejandro.gandia9@gmail.com>
2017-05-19 12:07:03 +0200

    genres style

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,20 @@
 var app = angular.module('gameApp')
 app.controller('DataController', function($scope, GamesFactory, $location) {
 	$scope.games = function(gameTitle) {
-				console.log("from controller.. we search by => " + $scope.gameTitle)
-				$scope.gamesInfo = GamesFactory.getGames($scope.gameTitle)
-					.then(function(response) {
-						$location.url("/games")
-						console.log(response.data.results)
-						$scope.gamesInfo = response.data.results
-						$scope.myStyle={display:'inline'}
-						
-					})
-					.catch(function(response) {
-						console.log(response.data);
-						$location.url("/error")
-					});
-					$scope.mySecStyle={transform: 'rotate(360deg)', transition: 'width 2s, height 2s, transform 2s'}
-				}
+		console.log("from controller.. we search by => " + $scope.gameTitle)
+		$scope.gamesInfo = GamesFactory.getGames($scope.gameTitle)
+		.then(function(response) {
+			$location.url("/games")
+			console.log(response.data.results)
+			$scope.gamesInfo = response.data.results
+			$scope.myStyle={display:'inline'}
+		})
+		.catch(function(response) {
+			console.log(response.data);
+			$location.url("/error")
+		});
+		$scope.mySecStyle={transform: 'rotate(360deg)', transition: 'width 2s, height 2s, transform 2s'}
+	}
 
 	$scope.gameMoreInfo = function(gameId){
 		$scope.gameId = gameId
@@ -24,7 +23,17 @@ app.controller('DataController', function($scope, GamesFactory, $location) {
 		.then(function(response) {
 			console.log(response.data.results)
 			$scope.infoAboutGames = response.data.results
-		});
+
+			for(prop in $scope.infoAboutGames){
+				if (prop === 'genres') {
+					for (var i = 0; i < $scope.infoAboutGames[prop].length; i++) {
+						console.log(`call ${i} times`)
+						$scope.infoAboutGames[prop][i].name = GamesFactory.getIcon($scope.infoAboutGames[prop][i].name)
+					};
+					
+				}
+			}
+		})
 	}
 
 	$scope.addToFavorite = function(name){
--- js/factory.js
+++ js/factory.js
@@ -14,7 +14,66 @@ angular.module('GamesService', [])
 				// 	console.log('Error: ' + data);
 				// });
 		}
+
+		var getIcon = function(infoAboutGames){
+			console.log(infoAboutGames)
+							if(infoAboutGames === 'Driving/Racing'){
+								return `🏎 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Puzzle'){
+								return  `⛏ ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Adventure'){
+								return `🏇🏼🎒 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Platformer'){
+								return `🏃🏽 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Action'){
+								return `🏹 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Shooter'){
+								return `🔫 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Role-Playing'){
+								return `🗡 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Fighting'){
+								return `👊🏼 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Vehicular Combat'){
+								return `🏎💣 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Action-Adventure'){
+								return `💥💣 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Strategy'){
+								return `🤔 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Real-Time Strategy'){
+								return `🤔⏳ ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Simulation'){
+								return `👥 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Flight Simulator'){
+								return `✈️ ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'MOBA'){
+								return `🌍 ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'Card Game'){
+								return `♠️♣️♥️♦️ ${infoAboutGames}`
+							}
+							else if(infoAboutGames === 'MMORPG'){
+								return `🌍🗡 ${infoAboutGames}`
+							}
+							else {
+								return `⁉️  ${infoAboutGames}`
+							}
+			}
 		return {
+			getIcon: getIcon,
 			getGames: getGames,
 			getInfo: getInfo
 		}

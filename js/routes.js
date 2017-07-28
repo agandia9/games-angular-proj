cd30b8a1b1cc8e427cd6ce4131a7c7d924080d01
agandia9 <alejandro.gandia9@gmail.com>
2017-07-28 11:16:56 +0200

    git-atom testing...

--- js/routes.js
+++ js/routes.js
@@ -1,14 +1,16 @@
 angular.module('gameApp')
-.config(['$routeProvider', function config( $routeProvider ) {
-	  $routeProvider
-			.when("/games", {
-				templateUrl: 'templates/gamesFound.html',
-			})
-			.when("/error", {
-				templateUrl: 'templates/error.html',
-			})
-			.when("/relacionatedGames", {
-				templateUrl: 'templates/relacionated_games.html',
-			})
-			.otherwise({ redirectTo: '/' })
-	}]);
+  .config(['$routeProvider', function config($routeProvider) {
+    $routeProvider
+      .when("/games", {
+        templateUrl: 'templates/gamesFound.html',
+      })
+      .when("/error", {
+        templateUrl: 'templates/error.html',
+      })
+      .when("/relacionatedGames", {
+        templateUrl: 'templates/relacionated_games.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      })
+  }]);
